d2549c0a3e7e3f4b42fdcdc9c2cf4bc8a88ea0ec
marvin <marvinnnz@gmail.com>
2015-04-15 11:00:45 -0400

    First Commit

---
+++ Snippets.cs
@@ -0,0 +1,15 @@
+      private void TxtUsuario_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+      {
+
+          var txt = (TextBox)sender;
+          txt.Text = CheckRegex(txt.Text, @"[\w+\d+]");
+          txt.CaretIndex = txt.Text.Length;
+      }
+
+      private static String CheckRegex(string text, string regex)
+      {
+          var regexResult = Regex.Matches(text, regex);
+          return text.Length == regexResult.Count ? text :
+              regexResult.Cast<object>().Aggregate("", (current, match) => current + match.ToString());
+
+      }
